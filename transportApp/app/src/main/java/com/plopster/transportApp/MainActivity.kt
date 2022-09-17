efd9c31218790cf558945c243dfc391445184e9d
Nicolas Estevez <esteveznicolas0@gmail.com>
2022-09-16 19:01:47 -0500

    Carga interfaz de logueo finalizada

--- transportApp/app/src/main/java/com/plopster/transportApp/MainActivity.kt
+++ transportApp/app/src/main/java/com/plopster/transportApp/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.plopster.transportApp
 
-import androidx.appcompat.app.AppCompatActivity
+//Main activity pantalla principal del proyecto que se muestra una vez se corre la aplicacion
+
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {//OnCreate pertenece al ciclo de vida de las aplicaciones android
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)// Instancia el contenido que instanciara la pantalla
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
     }
 }
