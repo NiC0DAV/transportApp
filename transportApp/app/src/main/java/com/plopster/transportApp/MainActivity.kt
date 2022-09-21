90341e4603fdf13834598e4e8ac76de8f8e6ab71
NicolasEstevez <esteveznicolas0@gmail.com>
2022-09-21 18:13:05 -0500

    Agrega validaciones de formulario de registro de usuario y a formulario de login

--- transportApp/app/src/main/java/com/plopster/transportApp/MainActivity.kt
+++ transportApp/app/src/main/java/com/plopster/transportApp/MainActivity.kt
@@ -6,6 +6,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.WindowManager
+import android.widget.Toast
 import com.plopster.transportApp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +18,37 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)// Instancia el contenido que instanciara la pantalla
-        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+            WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
         binding.registerBtn.setOnClickListener{ registerRedir() }
+
+        binding.loginButton.setOnClickListener{ loginUser() }
+    }
+
+    private fun loginUser(){
+        val email = binding.emailTextField.text.toString();
+        val password = binding.passwordTextField.text.toString();
+
+        if(validateForm(email, password)){
+            Toast.makeText(this, "Inicio de sesión exitoso", Toast.LENGTH_LONG).show()
+        }
+    }
+
+
+    private fun validateForm(email: String, password: String): Boolean{
+        if(email.isEmpty()){
+            Toast.makeText(this, "Debes ingresar un correo electronico",
+                Toast.LENGTH_LONG).show()
+            return false;
+        }
+
+        if(password.isEmpty()){
+            Toast.makeText(this, "Debes ingresar una contraseña",
+                Toast.LENGTH_LONG).show()
+            return false;
+        }
+        return true;
     }
 
     private fun registerRedir(){
--- transportApp/app/src/main/java/com/plopster/transportApp/RegisterActivity.kt
+++ transportApp/app/src/main/java/com/plopster/transportApp/RegisterActivity.kt
@@ -1,10 +1,10 @@
 package com.plopster.transportApp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
-import com.plopster.transportApp.databinding.ActivityMainBinding
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.plopster.transportApp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -17,8 +17,51 @@ class RegisterActivity : AppCompatActivity() {
         setContentView(binding.root)
         window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
 
-        binding.loginBtn.setOnClickListener{ loginRedirect() }
+        binding.loginBtn.setOnClickListener{ registerUser() }
+        binding.registerButton.setOnClickListener{ registerUser() }
     }
+
+    private fun registerUser(){
+        val name = binding.nameTextField.text.toString();
+        val lastName = binding.lastNameTextField.text.toString();
+        val phoneNumber = binding.phoneNumberTextField.text.toString();
+        val regEmail = binding.regEmailTextField.text.toString();
+        val regPassword = binding.regPasswordTextField.text.toString();
+        val regConfirmPassword = binding.confirmPasswordTextField.text.toString();
+        if(regPassword == regConfirmPassword){
+            val mapData = mapOf(
+                "Nombre" to name,
+                "Apellido" to lastName,
+                "Telefono" to phoneNumber,
+                "Correo Electronico" to regEmail,
+                "Contraseña" to regPassword,
+                "Confirmacion" to regConfirmPassword
+            );
+
+            if(formValidate(mapData)){
+                Toast.makeText(this, "Registro Exitoso", Toast.LENGTH_LONG).show();
+            }
+        }else{
+            Toast.makeText(this, "Los campos contraseña y confirmacion de " +
+                    "contraseña deben ser iguales", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private fun formValidate(mapData: Map<String, String>): Boolean{
+        var status = false;
+
+        for ((k, v) in mapData){
+            if(v == ""){
+                status = false
+                Toast.makeText(this, "El campo de $k no debe estar vacio", Toast.LENGTH_LONG).show();
+                break
+            }else{
+                status = true
+            }
+        }
+        return status
+    }
+
 
     private fun loginRedirect(){
         val i = Intent(this, MainActivity::class.java)
