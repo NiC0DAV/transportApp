d996818c4b36dbb3dccc865342bfa1a912dc0ad3
NicolasEstevez <esteveznicolas0@gmail.com>
2022-09-21 13:27:45 -0500

    Carga interfaz de registro y soluciona inconveniente presentado en commit anterior por version de graddle

--- transportApp/app/src/main/java/com/plopster/transportApp/RegisterActivity.kt
+++ transportApp/app/src/main/java/com/plopster/transportApp/RegisterActivity.kt
@@ -1,11 +1,28 @@
 package com.plopster.transportApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.plopster.transportApp.databinding.ActivityMainBinding
+import com.plopster.transportApp.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+
+        binding.loginBtn.setOnClickListener{ loginRedirect() }
     }
+
+    private fun loginRedirect(){
+        val i = Intent(this, MainActivity::class.java)
+        startActivity(i)
+    }
+
 }
